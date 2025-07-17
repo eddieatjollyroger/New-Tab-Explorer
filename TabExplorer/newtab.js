f5b6a5a35f64653b44238e9ed1b9731e904eeb3f
João Pacheco <38026492+eddieatjollyroger@users.noreply.github.com>
2025-07-17 22:56:50 +0000

    FIX empty shortcut input url

--- TabExplorer/newtab.js
+++ TabExplorer/newtab.js
@@ -339,7 +339,7 @@ function loadQuickShortcuts() {
 document.getElementById('addQuickShortcut').addEventListener('click', () => {
   const label = document.getElementById('shortcutLabel').value.trim();
   const urlTrim = document.getElementById('shortcutUrl').value.trim();
-  const url = prependHttps(urlTrim);
+  const url = urlTrim ? prependHttps(urlTrim): urlTrim;
 
   if (!label || !url) {
     alert('Please enter both label and URL.');
@@ -684,6 +684,7 @@ function startPlaceholderTyping() {
 function stopPlaceholderTyping() {
   if (!isTyping) return;
   clearInterval(placeholderInterval);
+  placeholderInterval = undefined; // actually clear Interval
   input.textContent = "";
   input.classList.remove("placeholder");
   placeholderIndex = 0;
